c22dadafef9610527a6f2c3a7ce4a0bc34015209
zhaoxuliang <826946484@qq.com>
2025-03-26 16:19:43 +0800

    component的方式配置授权服务

---
+++ ch02_component_config/src/main/java/top/jgblm/oauth_server/ch02/Ch02.java
@@ -0,0 +1,11 @@
+package top.jgblm.oauth_server.ch02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch02 {
+    public static void main(String[] args) {
+        SpringApplication.run(Ch02.class, args);
+    }
+}
